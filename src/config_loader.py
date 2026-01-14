4b2ee186ffa687ac67b445f74d3c7af69e51182c
Narendra Kashyap <thoughtbullswork@gmail.com>
2026-01-14 17:54:10 +0530

    restucture the code with config

---
+++ src/config_loader.py
@@ -0,0 +1,10 @@
+import yaml
+from typing import Dict
+
+BASE_PATH = "/Workspace/Repos/databricks-sales-pipeline"
+
+def load_config(env: str) -> Dict:
+    config_path = f"{BASE_PATH}/configs/{env}.yaml"
+
+    with open(config_path, "r") as f:
+        return yaml.safe_load(f)
