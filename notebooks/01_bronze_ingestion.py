71ac92360decfe372ceb426f6159518057b14b0f
Narendra Kashyap <thoughtbullswork@gmail.com>
2026-02-03 17:48:20 +0530

    separate transformation logic from notebook

--- notebooks/01_bronze_ingestion.py
+++ notebooks/01_bronze_ingestion.py
@@ -1,32 +1,26 @@
 # Databricks job parameter
 import argparse
+from pyspark.sql import SparkSession
+from src.config_loader import load_config
+from src.bronze import transform_orders_bronze
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+
+import sys
+sys.path.append("/Workspace/Shared/databricks-sales-pipeline")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--env", required=True)
 args = parser.parse_args()
-
 env = args.env
 
 
-import sys
-
-sys.path.append("/Workspace/Shared/databricks-sales-pipeline")
-
-from src.config_loader import load_config
-from pyspark.sql.functions import current_timestamp
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
-from pyspark.sql import SparkSession
+config_path = f"/Volumes/{env}_catalog/pipelines/configs/{env}.yaml"
+cfg = load_config(config_path)
 
 spark = SparkSession.builder \
     .appName("Sales Data Analysis") \
     .getOrCreate()
-
-config_path = f"/Volumes/{env}_catalog/pipelines/configs/{env}.yaml"
-# config_path = "/home/naren/Projects/databricks-sales-pipeline/configs/dev.yaml"
-
-# raw = spark.read.text(config_path)
-# raw_yaml = "\n".join([r.value for r in raw.collect()])
-cfg = load_config(config_path) 
+ 
 
 catalog = cfg["catalog"]
 bronze_schema = cfg["schema_bronze"]
@@ -40,17 +34,17 @@ order_schema = StructType([
     StructField("order_status", StringType(), True)
 ])
 
-df = (
+raw_df = (
  spark.read.schema(order_schema)
     .option("header", True)
     .csv(orders_file)
 )
 
-df.show()
+raw_df.show()
+bronze_df = transform_orders_bronze(raw_df)
 
-df.withColumn("ingesttime", current_timestamp()) \
+bronze_df.mode("overwrite") \
   .write.format("delta") \
-  .mode("overwrite") \
   .saveAsTable(f"{catalog}.{bronze_schema}.orders")
 
 print(f"Bronze load completed for env={env}")
---
+++ src/bronze.py
@@ -0,0 +1,15 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, current_timestamp
+
+def transform_orders_bronze(df: DataFrame)-> DataFrame:
+    """
+    RAW -> bronze transformation
+    """
+    bronze_df = df.dropDuplicates(["order_id"]) \
+    .withColumn("order_id", col("order_id").cast("int")) \
+    .withColumn("order_date", col("order_date").cast("date"))\
+    .withColumn("customer_id", col("customer_id").cast("int")) \
+    .withColumn("order_status", col("order_status").cast("string")) \
+    .withColumn("ingested_at", current_timestamp())
+    
+    return bronze_df
